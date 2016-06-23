f5f40ed96e537b2688f90495f6a139ba243ed49c
Andrey <andrey.bazhutkin@gmail.com>
2016-06-23 11:38:13 +1000

    NEP-2757 Filter verification

---
+++ src/helpers/FilterVerificator.js
@@ -0,0 +1,46 @@
+var uri = require("URIjs");
+
+export class FilterVerificator {
+  constructor(configuration) {
+    this.configurationFilters = configuration.filterBarConfiguration.filters;
+    this.urlFiltersJson = uri(window.location).query(true).q;
+  }
+
+  verify() {
+    if (this.urlFiltersJson == '') {
+      return true;
+    }
+
+    var urlFilters = JSON.parse(this.urlFiltersJson),
+        configurationMap = this.configurationFiltersMap();
+
+    for (var filter of urlFilters) {
+      var signature = this.filterSignature(filter.uid, filter);
+      
+      if (typeof configurationMap[signature] === 'undefined') {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  configurationFiltersMap() {
+    var map = {};
+
+    Object.keys(this.configurationFilters).map(function(filterUid) {
+      var filter = this.configurationFilters[filterUid],
+          signature = this.filterSignature(filterUid, filter);
+      
+      map[signature] = true;
+    }.bind(this));
+
+    return map;
+  }
+
+  filterSignature(uid, filter) {
+    return [uid, filter.type, filter.field].join(',');
+  }
+}
+
+
