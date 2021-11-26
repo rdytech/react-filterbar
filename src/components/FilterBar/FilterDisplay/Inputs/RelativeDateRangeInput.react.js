1a2b352af5fb8d14e10fc5e9f0dc8d074264c636
Emerson Xavier <emerson.xavier@readytech.io>
2021-11-26 14:13:35 +1100

    [NEP-12470] review comments and refactor

--- src/components/FilterBar/FilterDisplay/Inputs/RelativeDateRangeInput.react.js
+++ src/components/FilterBar/FilterDisplay/Inputs/RelativeDateRangeInput.react.js
@@ -1,11 +1,50 @@
-import { RangeInput } from "./RangeInput.react";
 import { DateInput } from "./DateInput.react";
-export class RelativeDateRangeInput extends RangeInput {
+export class RelativeDateRangeInput extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: this.props.value || { from: null, to: null },
-      operator: this.props.value.operator || "absolute"
+      value: this.initRelativeValues(),
+      operator: this.props.value.operator || "absolute",
+    }
+  }
+
+  initRelativeValues() {
+    if (!this.props.value.operator === "absolute") {
+      var relativeOption = this.props.value
+      if (typeof this.props.value === 'object') {
+        relativeOption = this.props.value.value
+      }
+
+      return relativeValues(relativeOption)
+    } else {
+      return this.props.value
+    }
+  }
+
+  relativeValues(relativeOption){
+    switch (relativeOption) {
+      case "This Week":
+        return {
+          value: relativeOption,
+          operator: this.props.value.operator,
+          from: moment().startOf('isoWeek').diff(moment(), 'days'),
+          to: moment().endOf('isoWeek').diff(moment(), 'days')
+        }
+      case "Last Week":
+        var lastWeek = moment().subtract(1, 'week');
+        return {
+          value: relativeOption,
+          operator: this.props.value.operator,
+          from: lastWeek.clone().startOf('isoWeek').diff(moment(), 'days'),
+          to: lastWeek.clone().endOf('isoWeek').diff(moment(), 'days')
+        }
+      default:
+        return {
+          value: relativeOption,
+          operator: this.props.value.operator,
+          from: null,
+          to: null
+        }
     }
   }
 
@@ -21,7 +60,7 @@ export class RelativeDateRangeInput extends RangeInput {
     if (value < 0) {
       return moment().subtract(Math.abs(value), 'day')
     } else {
-      return moment().add(Math.abs(value), 'day')
+      return moment().add(value, 'day')
     }
   }
 
@@ -35,19 +74,27 @@ export class RelativeDateRangeInput extends RangeInput {
     }
   }
 
-  onChangeFrom(event) {
+  handleInputChange(event, input) {
     var newValue = this.state.value
-    newValue["from"] = event.target.value
+    newValue[input] = event.target.value
+    newValue['value'] = "Custom" // Reset preset selection
     this.setState({ value: newValue })
   }
 
-  onChangeTo(event) {
+  handleDatePickerChange(event) {
     var newValue = this.state.value
-    newValue["to"] = event.target.value
-    this.setState({ value: newValue })
+    newValue['value'] = "Custom" // Reset preset selection
+
+    if (event.type === "dp") {
+      newValue[event.target.querySelector("input").getAttribute("placeholder")] = event.target.querySelector("input").value;
+    } else if (event.type === "input") {
+      newValue[event.target.getAttribute("placeholder")] = event.target.value;
+    }
+
+    this.setState({ value: newValue });
   }
 
-  onRelativeChange(event) {
+  onPresetChange(event) {
     var selectedOption = $(event.target.childNodes[event.target.selectedIndex]);
     var fromValue = selectedOption.context.dataset.from
     var toValue = selectedOption.context.dataset.to
@@ -88,7 +135,7 @@ export class RelativeDateRangeInput extends RangeInput {
   showDateInputs() {
     return (
       <div>
-        <DateInput value={this.state.value} filterUid={this.props.filterUid} displayFrom={this.state.displayFrom} displayTo={this.state.displayTo} />
+        <DateInput value={this.state.value} filterUid={this.props.filterUid} displayFrom={this.state.displayFrom} displayTo={this.state.displayTo} onDateChangeCustom={this.handleDatePickerChange} />
       </div>
     )
   }
@@ -103,8 +150,7 @@ export class RelativeDateRangeInput extends RangeInput {
           <input
             type="number"
             className="form-control"
-            onBlur={this.onBlur.bind(this)}
-            onChange={(e) => this.onChangeFrom(e)}
+            onChange={(e) => this.handleInputChange(e, 'from')}
             placeholder="+/- days"
             value={this.state.value.from}
           />
@@ -122,8 +168,7 @@ export class RelativeDateRangeInput extends RangeInput {
           <input
             type="number"
             className="form-control"
-            onBlur={this.onBlur.bind(this)}
-            onChange={(e) => this.onChangeTo(e)}
+            onChange={(e) => this.handleInputChange(e, 'to')}
             placeholder="+/- days"
             value={this.state.value.to}
           />
@@ -140,18 +185,15 @@ export class RelativeDateRangeInput extends RangeInput {
 
   render() {
     return (
-      <div className="col-sm-10">
-        <select className="form-control"
-          onChange={this.onRelativeChange.bind(this)}
-          value={this.state.value.value}
-        >
+      <div className="col-xl-10">
+        <select className="form-control" onChange={this.onPresetChange.bind(this)} value={this.state.value.value}>
           {Object.keys(this.props.relativeOptions).map((optionKey) => (
             this.relativeOption(optionKey)
           ))}
         </select>
 
         {
-          this.state.operator == "absolute" ? this.showDateInputs() : this.showRelativeRangeInputs()
+          this.isAbsolute() ? this.showDateInputs() : this.showRelativeRangeInputs()
         }
 
         <div className="input-group">
@@ -171,7 +213,7 @@ export class RelativeDateRangeInput extends RangeInput {
               type="radio"
               name="operator"
               value="relative"
-              checked={this.state.operator == "relative"}
+              checked={!this.isAbsolute()}
               onChange={(e) => this.setState({ operator: e.target.value })}
             />
             Relative
@@ -181,6 +223,11 @@ export class RelativeDateRangeInput extends RangeInput {
     );
   }
 }
+
+RelativeDateRangeInput.contextTypes = {
+  filterBarActor: React.PropTypes.object.isRequired,
+  filterBarStore: React.PropTypes.object.isRequired
+};
 
 RelativeDateRangeInput.defaultProps = {
   dateFormat: 'DD/MM/YYYY',
