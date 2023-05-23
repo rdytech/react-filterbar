2a26edc8429593ac2c7916ce6871a39d98531729
Thanh Huynh <thanh.huynh@readytech.io>
2023-05-23 13:24:48 +0700

    [NEP-15635] Code review

--- dist/react-filterbar.js
+++ dist/react-filterbar.js
@@ -22828,6 +22828,7 @@ module.exports={
     }
   }
 }
+
 },{}],481:[function(require,module,exports){
 "use strict";
 
@@ -22846,12 +22847,13 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+I18nJs.translations["en"] = defaultTranslations["en"];
+
 function t(key) {
   if (typeof I18n !== 'undefined') {
     return I18n.t(key);
   }
 
-  I18nJs.translations["en"] = defaultTranslations["en"];
   return I18nJs.t(key);
 }
 
--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,9 +1,10 @@
 import * as defaultTranslations from "./en/translations.json";
 import * as I18nJs from "i18n-js";
 
+I18nJs.translations["en"] = defaultTranslations["en"];
+
 function t(key){
   if (typeof I18n !== 'undefined'){ return I18n.t(key); }
-  I18nJs.translations["en"] = defaultTranslations["en"];
   return I18nJs.t(key);
 }
 
